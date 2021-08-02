6ac9a557e562c9b3d05eb9500fed930b3cded6d9
Quinnp168 <59508579+Quinnp168@users.noreply.github.com>
2021-08-01 18:21:38 -0700

    Update blinking_lights.ino
    change to array.

--- blinking_lights/blinking_lights.ino
+++ blinking_lights/blinking_lights.ino
@@ -1,22 +1,19 @@
-int LED1 = 13;
-int LED2 = 12;
-int LED3 = 11;
+int LED[] = {13, 12 11};
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
+  pinMode(LED[0],OUTPUT);
+  pinMode(LED[1],OUTPUT);
+  pinMode(LED[2],OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(LED1,HIGH);
-digitalWrite(LED3,LOW);
+digitalWrite(LED[0],HIGH);
+digitalWrite(LED[2],LOW);
 delay(200);
-digitalWrite(LED1,LOW);
-digitalWrite(LED2,HIGH);
+digitalWrite(LED[0],LOW);
+digitalWrite(LED[1],HIGH);
 delay(200);
-digitalWrite(LED2,LOW);
-digitalWrite(LED3,HIGH);
+digitalWrite(LED[1],LOW);
+digitalWrite(LED[2],HIGH);
 delay(200);
 }
