6bd4d72d53c192e76d90f3b57ee13f4077910588
William Pursell <william.r.pursell@gmail.com>
2021-08-02 02:00:24 +0000

    Add indentation

--- buzzer/buzzer/buzzer.ino
+++ buzzer/buzzer/buzzer.ino
@@ -1,26 +1,26 @@
 int buzz = 12;
 
 void setup() {
-pinMode(buzz,OUTPUT);
+	pinMode(buzz,OUTPUT);
 }
 
 void loop() {
-tone(buzz, 475);
-delay(200);
-noTone(buzz);
-delay(175);
-tone(buzz, 200);
-delay(200);
-noTone(buzz);
-tone(buzz,420);
-delay(200);
-noTone(buzz);
-delay(100);
-tone(buzz,545);
-delay(200);
-noTone(buzz);
-delay(50);
-tone(buzz, 425);
-delay(300);
-noTone(buzz);
+	tone(buzz, 475);
+	delay(200);
+	noTone(buzz);
+	delay(175);
+	tone(buzz, 200);
+	delay(200);
+	noTone(buzz);
+	tone(buzz,420);
+	delay(200);
+	noTone(buzz);
+	delay(100);
+	tone(buzz,545);
+	delay(200);
+	noTone(buzz);
+	delay(50);
+	tone(buzz, 425);
+	delay(300);
+	noTone(buzz);
 }
